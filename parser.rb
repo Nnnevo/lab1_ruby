35fa9f407919bddb4ebf69ebd96ad8eb3c020ddc
Nnnevo <dima-anton@ukr.net>
2022-12-16 20:40:59 +0200

    add working parse

--- parser.rb
+++ parser.rb
@@ -9,3 +9,20 @@ DOC = Nokogiri::HTML(HTML)
 
 
 
+smart_list=DOC.css("#p-list-category-products > div.products-listing.grid").css('.products-listing-item').slice(0,10)
+
+json_list=[]
+csv_list=[['brand','category','id','name','price']]
+
+
+
+smart_list.each do |obj| 
+    data_hash=JSON.parse(obj['data-ec']).sort.to_h
+    json_list.push(data_hash)
+    csv_list.push(data_hash.values)
+end
+
+
+
+File.write('smartphones.csv',csv_list.map(&:to_csv).join)
+File.write('smartphones.json',JSON.dump(json_list))
